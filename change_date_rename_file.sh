1ae6889d938bba1d90e04f7c6532b2611a02e89f
Kim Lew <kimlew451@gmail.com>
2019-09-24 10:55:59 -0600

    Assign value strings for date change & filename change to variables

--- change_date_rename_file.sh
+++ change_date_rename_file.sh
@@ -25,31 +25,25 @@ file_name="IMG_0061.jpg" # IMG_0059.jpg
 
 # TODO: Add loop - to process entire given directory.
 # while read filename; do
-# date_for_date_change= 
-# date_for_filename_change= 
 
-# Change filesystem date to EXIF photo-taken date. 
-identify -format '%[EXIF:DateTimeOriginal]' \
+# Change filesystem date to EXIF photo-taken date.
+# Save partial output of tee in variable, date_for_date_change.
+date_for_date_change=$(identify -format '%[EXIF:DateTimeOriginal]' \
 $directory_name/$file_name \
 | sed -e 's/://g' -e 's/ //g' -E -e 's/(..)$/\.\1/' \
-| tee $directory_name/date_manipulation/date_for_date_change.txt
+| tee $directory_name/date_manipulation/date_for_date_change.txt)
 
-# Save partial output of tee in variable, date_for_date_change.
-
-# touch -t 201509020709.06 $test_dirname/$test_filename
 # TODO: Replace with: touch -t 201509020709.03 $test_dirname\IMG_0059.jpg
-# NEED: Variable with date string for date change.
-# Shows OK in Finder. 
-# **But in identify output: Properties: date:create: 2019-09-18T18:55:21+00:00
+# touch -t 201509020709.06 $test_dirname/$test_filename
 
-# Use EXIF photo-taken date, change format and use as part of filename. 
-identify -format '%[EXIF:DateTimeOriginal]' \
+# Use EXIF photo-taken date, change format and use as part of filename.
+# Save partial output of tee in variable, date_for_filename_change.
+date_for_filename_change=$(identify -format '%[EXIF:DateTimeOriginal]' \
 $directory_name/$file_name \
 | sed -e 's/.\{3\}$//' -e 's/:/-/g' -e 's/ /_/g' \
-| tee $directory_name/date_manipulation/date_for_filename_change.txt
+| tee $directory_name/date_manipulation/date_for_filename_change.txt)
 
 # TODO: Replace in filename IMG_ with $datestring_for_filename.
-# NEED: Variable with date string for filename change.
 
 # done
 
